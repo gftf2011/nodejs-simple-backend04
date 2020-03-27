50bfbae1f44a5b4e7b807d400b4bf179bc0ac552
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2020-03-27 20:49:18 -0300

    Mudança de HOST em database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.103', // IP given by your docker machine OR your localhost
+  host: '127.0.0.1', // IP given by your docker machine OR your localhost
   username: 'postgres',
   password: 'docker',
   database: 'users_database',
